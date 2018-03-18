97c892212b005c5401e87690d6ab8afbfc1e5df9
Jessica M <contact@jmvazquez.com>
2018-03-18 12:32:49 -0700

    Timed the particles to self-destroy so they don't end up lying around

---
+++ Assets/Scripts/TimedSelfDestroy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedSelfDestroy : MonoBehaviour
+{
+    public float destroyTime = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(gameObject, destroyTime);
+    }
+
+}
